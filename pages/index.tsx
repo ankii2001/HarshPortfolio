125dd22f230fa98d7889f5a469ec46b257037731
ankii2001 <ankitluthra2001@gmail.com>
2022-09-30 16:53:37 +0530

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { GetStaticProps } from "next";
+import type { NextPage, GetStaticProps } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import About from "../components/About";
@@ -25,7 +25,8 @@ type Props = {
   socials: Social[];
 };
 
-const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
+const Home: NextPage<Props> = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
+// const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
   return (
     <div
       className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scroll-smooth scrollbar scrollbar-track-gray-400/5
