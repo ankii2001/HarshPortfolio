e4d5dffad48568782f107c8aaebbdd0cc543a5f9
ankii2001 <ankitluthra2001@gmail.com>
2022-10-10 17:26:17 +0530

    Added meta data to the file to make it easier to find the file in the future.

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
     >
       <Head>
         <title>{pageInfo?.name}</title>
-        <meta name="description" content="AnkitLuthraPortfolio"/>
+        <meta name="description" content="Ankit_Luthra_Portfolio"/>
         <link rel="icon" href="/AnkiiGit.png" />
       </Head>
 
