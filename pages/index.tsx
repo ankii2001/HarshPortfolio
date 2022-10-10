c23f5e791628de0cb3b7fab5df03e1b40074ed45
ankii2001 <ankitluthra2001@gmail.com>
2022-10-10 17:23:12 +0530

    added meta data

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ const Home = ({ pageInfo, experiences, projects, skills, socials }: Props) => {
     >
       <Head>
         <title>{pageInfo?.name}</title>
-        <meta name="description" content="AnkitLuthraPortfolio"/>
+        <meta name="description" content=""/>
         <link rel="icon" href="/AnkiiGit.png" />
       </Head>
 
